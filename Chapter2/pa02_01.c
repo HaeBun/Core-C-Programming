3419277786c34e1fd5a66855df0593cc08a2be0b
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-04-25 17:00:50 +0900

    초기 커밋.

---
+++ Chapter2/pa02_01.c
@@ -0,0 +1,17 @@
+/*
+1. 다음과 같이 출력하는 프로그램을 작성하시오. 단, printf문은 한 번만 사용한다.
+
+실행 결과
+int main(void)
+{
+	return 0;
+}
+
+*/
+
+#include <stdio.h>
+
+int pa02_01()
+{
+	printf("int main(void) \n{ \n\tretrun 0; \n}");
+}
