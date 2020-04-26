0dab4eb4c4ee38928f55f07e7d8d8cdde472a5df
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-04-27 01:47:51 +0900

    [산술연산자/난이도 ★★]

---
+++ Chapter4/pa04_06.c
@@ -0,0 +1,28 @@
+/*
+6. 점 A의 좌표 (x1, y1)과 점 B의 좌표 (x2, y2)를 입력받아 두 점 A, B를 지나는 직선의 기울기를 구하는 프로그램을 작성하시오.
+
+m = (y2 - y1 / x2 - x1)
+
+실행결과
+한 점의 좌표 (x1, y1)? [10 20]
+또 다른 점의 좌표 (x2, y2)? [40 50]
+직선의 기울기: 1.000000
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int x1, y1, x2, y2;
+	float result;
+	printf("한 점의 좌표 (x1, y1)? ");
+	scanf("%d %d", &x1, &y1);
+
+	printf("또 다른 점의 좌표 (x2, y2)? ");
+	scanf("%d %d", &x2, &y2);
+
+	result = (float)(y2 - y1) / (float)(x2 - x1);
+
+	printf("직선의 기울기: %f", result);
+}
