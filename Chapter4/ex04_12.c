ebf61661ecedff59d4fc059d5884031b9f862d61
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-04-27 00:01:30 +0900

    예제 4-12 : 조건 연산자의 사용 예

---
+++ Chapter4/ex04_12.c
@@ -0,0 +1,27 @@
+// 조건 연산자의 사용 예
+
+#define _CRT_SECURE_NO_WARNINGS
+
+int ex04_12(void)
+{
+	int a, b;
+	int result, max;
+
+	printf("2개의 정수? ");
+	scanf("%d %d", &a, &b);
+
+	printf("%d는 ", a);
+	a % 2 != 0 ? printf("홀수") : printf("짝수");
+	printf("입니다.\n");
+
+	result = a > 0 ? a : -a;
+	printf("a의 절대값: %d\n", result);
+
+	result = b > 0 ? b : -b;
+	printf("b의 절대값: %d\n", result);
+
+	max = a > b ? a : b;
+	printf("a, b 중 큰 값: %d\n", max);
+
+	return 0;
+}
