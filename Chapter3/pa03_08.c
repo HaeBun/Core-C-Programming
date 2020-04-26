aa5175ba896f60c43e8571b9a25b9d50a669455e
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-04-26 13:32:50 +0900

    [상수/난이도 ★]

---
+++ Chapter3/pa03_08.c
@@ -0,0 +1,25 @@
+/*
+8. 길이를 야드(yd)로 입력받아 미터(m)로 변환해서 출력하는 프로그램을 작성하시오.
+1yd는 0.9144m에 해당한다.
+
+실행결과
+길이(yd)? [100]
+100 yd = 91.440000 m
+
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#define ydToMiter 0.9144
+#include <stdio.h>
+
+int pa03_08(void)
+{
+	int yard;
+	double miter;	//float으로 받았을 때 할당된 바이트수가 적어 정밀도가 떨어짐.
+	printf("길이(yd)? ");
+	scanf("%d", &yard);
+	
+	miter = yard * ydToMiter;
+
+	printf("%d yd = %f m", yard, miter);
+}
