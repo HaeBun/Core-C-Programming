d5a84581d00abf7f087a094d926b3c167d84bcee
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-04-26 23:41:17 +0900

    예제 4-10 : 비트 논리 연산자의 사용 예

--- Chapter4/ex04_10.c
+++ Chapter4/ex04_10.c
@@ -8,10 +8,10 @@ int main(void)
 	unsigned short x = 0x12;
 	unsigned short y = 0xF0;
 
-	printf("%08x & %08x = %08x\n", x, y, x & y);
-	printf("%08x | %08x = %08x\n", x, y, x | y);
-	printf("%08x ^ %08x = %08x\n", x, y, x ^ y);
-	printf("~%08x = %08x\n", x, y, ~x);
+	printf("%08x & %08x = %08x\n", x, y, x & y);	// 비트 AND
+	printf("%08x | %08x = %08x\n", x, y, x | y);	// 비트 OR
+	printf("%08x ^ %08x = %08x\n", x, y, x ^ y);	// 비트 XOR
+	printf("~%08x = %08x\n", x, y, ~x);				// 비트 NOT
 
 	return 0;
 }
