45103342c1155e1619e03477e0dad10e96540c50
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-05-09 23:32:58 +0900

    예제 5-4 : 중첩된 if의 사용 예

---
+++ Chapter5/ex05_04.c
@@ -0,0 +1,26 @@
+// 중첩된 if의 사용 예
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+int main(void)
+{
+	int age, fee;
+
+	printf("나이? ");
+	scanf("%d", &age);
+
+	if (age >= 8) {
+		if (age >= 65) {
+			fee = 5000;
+		}
+		else {
+			fee = 10000;
+		}
+	}
+	else {
+		fee = 0;
+	}
+	printf("입장료: %d원\n", fee);
+	
+	return 0;
+}
