6f695695b409bd4b542497959bd9ae4a7f0ee3cd
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-05-10 00:24:52 +0900

    예제 5-9 : switch를 이용한 사칙연산 계산기

---
+++ Chapter5/ex05_09.c
@@ -0,0 +1,35 @@
+// switch를 이용한 사칙연산 계산기
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+int ex05_09(void)
+{
+	int a, b;			// 피연산자
+	char op;			// 연산자 기호를 문자로 저장할 변수
+
+	printf("수식? ");
+	scanf("%d %c %d", &a, &op, &b);	// 10 + 30 형태로 입력받는다.
+
+	switch (op) {
+	case '+':
+		printf("%d + %d = %d\n", a, b, a + b);
+		break;
+	case '-':
+		printf("%d - %d = %d\n", a, b, a - b);
+		break;
+	case '*':
+		printf("%d * %d = %d\n", a, b, a * b);
+		break;
+	case '/':
+		if (b != 0)
+			printf("%d / %d = %.2f\n", a, b, (double)a / b);
+		else
+			printf("0으로 나눌 수 없습니다.\n");
+		break;
+	default:
+		printf("잘못된 수식입니다.\n");
+		break;
+	}
+
+	return 0;
+}
