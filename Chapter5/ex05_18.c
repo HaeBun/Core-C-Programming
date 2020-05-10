6b8e26ae44b2c7ab4adcb980989d32fbe35b9774
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-05-10 18:48:44 +0900

    예제 5-18 : 무한 루프를 이용한 메뉴 처리

---
+++ Chapter5/ex05_18.c
@@ -0,0 +1,36 @@
+// 무한 루프를 이용한 메뉴 처리
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int ex05_18(void)
+{
+	int menu;
+	char filename[32] = "test.avi";
+
+	while (1)
+	{
+		printf("1.파일 열기\n");
+		printf("2.재생\n");
+		printf("3.재생 옵션\n");
+		printf("선택: ");
+
+		scanf("%d", &menu);
+		switch (menu) {
+		case 1:
+			printf("재생할 파일 이름? ");
+			scanf("%s", filename);
+			break;
+		case 2:
+			printf("%s를 재생합니다.\n", filename);
+			break;
+		case 3:
+			printf("재생 옵션을 선택합니다.\n");
+			break;
+		default:
+			printf("잘못 선택하셨습니다.\n");
+			break;
+		}
+	}
+
+	return 0;
+}
