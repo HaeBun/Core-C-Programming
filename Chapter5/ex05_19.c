bb89a66aabcf0ecba1042470864f586c152f8e65
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-05-10 18:49:02 +0900

    예제 5-19 : break의 사용 예

---
+++ Chapter5/ex05_19.c
@@ -0,0 +1,18 @@
+// break의 사용 예
+
+#include <stdio.h>
+
+int ex05_19(void)
+{
+	int i;
+
+	for (i = 10; i > 0; i--)
+	{
+		if (i % 3 == 0)
+			break;
+		printf("%d ", i);
+	}
+	printf("\n");
+
+	return 0;
+}
