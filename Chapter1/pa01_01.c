764a63c7e5b35fd4390b5d5cd9a7e1ac86feacb3
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-05-15 03:40:41 +0900

    [난이도 Null]

---
+++ Chapter1/pa01_01.c
@@ -0,0 +1,25 @@
+/*
+1. Visual Studio를 이용해서 다음과 같이 C 프로그램을 작성하고, 빌드 후 실행하시오.
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("FirstApp 프로그램\n");
+	printf("작성자: 천정아\n");
+	printf("버전: 1.0\n");
+
+	return 0;
+}
+
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	printf("FirstApp 프로그램\n");
+	printf("작성자: 김인태\n");
+	printf("버전: 1.0\n");
+
+	return 0;
+}
