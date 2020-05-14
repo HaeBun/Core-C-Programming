844628e5a4deda0facb0108546684eb4a0274983
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-05-15 03:35:35 +0900

    에제 6-12 : 전역 변수의 선언 및 사용

---
+++ Chapter6/ex06_12.c
@@ -0,0 +1,42 @@
+// 전역 변수의 선언 및 사용
+
+#include <stdio.h>
+
+void print_count(void);
+void increment(void);
+void decrement(void);
+
+int count;		// 전역 변수 선언
+
+int ex06_12(void)
+{
+	int i;
+
+	count = 0;
+	print_count();
+
+	for (i = 0; i < 3; i++)
+		increment();
+	print_count();
+
+	for (i = 0; i < 3; i++)
+		decrement();
+	print_count();
+
+	return 0;
+}
+
+void print_count(void)
+{
+	printf("count = %d\n", count);
+}
+
+void increment(void)
+{
+	count++;
+}
+
+void decrement(void)
+{
+	count--;
+}
