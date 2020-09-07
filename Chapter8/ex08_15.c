43b87443dcc8d1526da93278b69e1234dadcbab6
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-09-07 20:01:40 +0900

    예제 8-15 : 배열을 입력 매개변수로 사용하는 함수

--- Chapter8/ex08_15.c
+++ Chapter8/ex08_15.c
@@ -4,7 +4,7 @@
 void copy_array(const int source[], int target[], int size);
 void print_array(const int arr[], int size);
 
-int main(void)
+int ex08_15(void)
 {
 	int x[SIZE] = { 10, 20, 30, 40, 50 };
 	int y[SIZE] = { 0 };
