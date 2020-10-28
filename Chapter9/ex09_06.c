4c849b70f3957d1992f3fd107fbbc40b9017c9e8
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-10-28 19:13:17 +0900

    에제 9-6 : 문자열의 검색

---
+++ Chapter9/ex09_06.c
@@ -0,0 +1,20 @@
+// 에제 9-6 : 문자열의 검색
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int ex09_06(void)
+{
+	char filename[] = "readme.txt";
+	char *p = NULL;
+
+	p = strchr(filename, '.');
+	if (p != NULL)
+		printf("file extension: %s\n", p + 1);
+
+	p = strstr(filename, ".txt");
+	if (p != NULL)
+		printf("file type: TEXT file\n");
+
+	return 0;
+}
