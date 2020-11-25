0e3c67bfd0daedc54304ccf623d04a633d291f45
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-25 18:34:01 +0900

    예제 10-10 : 구조체를 포인터로 전달하는 경우

--- Chapter10/ex10_10.c
+++ Chapter10/ex10_10.c
@@ -7,7 +7,7 @@ typedef struct point
 	int x, y;		// 점의 좌표
 } POINT;
 
-void print_point(POINT* pt);
+void print_point_2(POINT* pt);
 
 int main(void)
 {
@@ -19,7 +19,7 @@ int main(void)
 
 	for (i = 0; i < sz; i++)
 	{
-		print_point(&arr[i]);	// 구조체 변수의 주소를 전달한다. 
+		print_point_2(&arr[i]);	// 구조체 변수의 주소를 전달한다. 
 		printf(" ");
 	}
 	printf("\n");
@@ -27,7 +27,7 @@ int main(void)
 	return 0;
 }
 
-void print_point(POINT* pt)		// 포인터에 의한 전달
+void print_point_2(POINT* pt)		// 포인터에 의한 전달
 {
 	printf("(%d, %d)", pt->x, pt->y);
 }
